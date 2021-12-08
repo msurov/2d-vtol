c4d1ecd86907476951192f1c6642e0b0e09c07bb
Maksim Surov <surov.m.o@gmail.com>
2021-12-08 13:19:10 +0300

    upt

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from dynamics import Dynamics, parameters, get_inv_dynamics
 
 trajfile = 'data/traj.npy'
 dynamics = Dynamics(parameters)
-# find_singular_trajectory.main(dynamics, trajfile)
+find_singular_trajectory.main(dynamics, trajfile)
 traj = find_singular_trajectory.load_trajectory(trajfile)
-transverse_linearization.get_trans_lin(dynamics, traj)
+# transverse_linearization.get_trans_lin(dynamics, traj)
